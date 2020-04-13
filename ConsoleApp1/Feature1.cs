e5595221b7aaf9de7ea84c4fc193b7018dffaf6f
Gabriel Dianu <gabriel_dianu@trimble.com>
2020-04-13 09:45:01 +0300

    add feature 1

---
+++ ConsoleApp1/Feature1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Feature1
+    {
+        public void WriteInConsole(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
