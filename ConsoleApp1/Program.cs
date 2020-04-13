a6138e65426daf32eebf64d07a0111f4f5d47b1f
Gabriel Dianu <gabriel_dianu@trimble.com>
2020-04-13 10:08:44 +0300

    added to feature 1 from WhiteThemedUser

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,11 +6,11 @@ namespace ConsoleApp1
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
             var feature1 = new Feature1();
 
             feature1.WriteInConsole("This is a demo . I am kind of bored");
+
+            feature1.WriteNumberInConsole(45);
         }
     }
 }
