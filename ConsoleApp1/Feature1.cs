e64e34e6ee289c2c6ac4798d976439389b008220
Gabriel Dianu <gabriel_dianu@trimble.com>
2020-04-13 11:09:14 +0300

    inital commit

---
+++ ConsoleApp1/Feature1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Feature1
+    {
+        public void WriteInConsole(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
